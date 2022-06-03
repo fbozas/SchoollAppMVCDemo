7546b4a55f6d911003e93d3a58d015f9c3fd0095
Panos Bozas <fbozas@hotmail.com>
2022-06-03 20:42:45 +0300

    Added Models and their relationships

---
+++ SchoollAppMVCDemo/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SchoollAppMVCDemo.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+        public List<Trainer> Trainers { get; set; }
+    }
+}
---
+++ SchoollAppMVCDemo/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace SchoollAppMVCDemo.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+    }
+}
