9945545d05f2a95a91ee4805d7dda5033141fb81
Panos Bozas <fbozas@hotmail.com>
2022-06-03 21:24:26 +0300

    Added CoursesController with GetAll feature and geBbyId

---
+++ SchoollAppMVCDemo/Controllers/CoursesController.cs
@@ -0,0 +1,31 @@
+﻿using SchoollAppMVCDemo.Services;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SchoollAppMVCDemo.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Index()
+        {
+            var courses = CourseService.Courses;
+            return View(courses);
+        }
+
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var course = CourseService.Courses.First(c => c.ID == id);
+                return View(course);
+            }
+            catch(Exception ex)
+            {
+                TempData["ErrorMessage"] = $"No course with ID {id.ToString()} found";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
